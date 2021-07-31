723b7247ea1bd9ec7195ef230652a94171209157
lichy <lichyseter@hotmail.com>
2021-07-31 19:42:03 +0800

    提交测试combinelatest 测试代码

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {combineLatest, interval, Subscription} from 'rxjs';
+import {take, tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'rxjs-test';
+  testSubscription: Subscription = new Subscription;
+
+
+  handleStart() {
+    const firstTimer = interval(1000).pipe(tap((t) => console.log("fist:" + t)), take(1));
+    const secondTimer = interval(500).pipe(tap((t) => console.log("second:" + t)), take(1));
+    this.testSubscription = combineLatest([firstTimer, secondTimer]).subscribe((value: any) => {
+      console.log(value);
+    });
+  }
+
+  handleEnd() {
+    this.testSubscription.unsubscribe();
+  }
 }
